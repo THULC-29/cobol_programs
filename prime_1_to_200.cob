42b86b756efc2fc5c939bf520eb50a5703e6cb29
THULC-29 <sthulasi224@gmail.com>
2026-02-05 08:17:41 +0530

    Add COBOL program to print prime numbers from 1 to 200

---
+++ prime_1_to_200.cob
@@ -0,0 +1,27 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PRIME200.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  NUM        PIC 9(3).
+       01  I          PIC 9(3).
+       01  DIV-COUNT  PIC 9(3).
+
+       PROCEDURE DIVISION.
+           DISPLAY "Prime numbers from 1 to 200:".
+
+           PERFORM VARYING NUM FROM 2 BY 1 UNTIL NUM > 200
+               MOVE 0 TO DIV-COUNT
+
+               PERFORM VARYING I FROM 1 BY 1 UNTIL I > NUM
+                   IF FUNCTION MOD(NUM, I) = 0
+                       ADD 1 TO DIV-COUNT
+                   END-IF
+               END-PERFORM
+
+               IF DIV-COUNT = 2
+                   DISPLAY NUM
+               END-IF
+           END-PERFORM.
+
+           STOP RUN.
